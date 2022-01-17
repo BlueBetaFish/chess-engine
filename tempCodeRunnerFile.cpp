04b5ae7c6e8acb253324365fd68a5566dc2ad154
Sourashis Mondal <sourashismondal872001@gmail.com>
2022-01-17 17:41:39 +0530

    removed tempCodeRunner

--- tempCodeRunnerFile.cpp
+++
@@ -1,9 +0,0 @@
-out << "{ ";
-    for (int squareIndex = 0; squareIndex < 64; squareIndex++)
-    {
-
-        cout << maskKnightAttacks(squareIndex).getDecimalValue() << "ULL";
-        if (squareIndex != 63)
-            cout << ", ";
-    }
-    c
