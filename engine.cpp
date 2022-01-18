6e918a54c6d87dd56bdb8219e089defa087c1b22
Sourashis Mondal <sourashismondal872001@gmail.com>
2022-01-18 18:37:18 +0530

    bishop and rook attack table

--- engine.cpp
+++ engine.cpp
@@ -84,16 +84,28 @@ enum
 
 //*----------------------------------------helper functions--------------------------------------------
 
-char getFileOfSquareIndex(int index)
+char inline getFileOfSquareIndex(int index)
 {
     return 'a' + index % 8;
 }
 
-int getRankOfSquareIndex(int index)
+int inline getRankOfSquareIndex(int index)
 {
     return 8 - (index / 8);
 }
-string getAlgebraicCoordinateFromIndex(int index)
+
+int inline getIndexFromRankAndFile(int rank, char file)
+{
+    return (8 - rank) * 8 + (file - 'a');
+}
+
+// row and column numbers are 0 based
+int inline getIndexFromRowAndColumnNumbers(int row, int column)
+{
+    return row * 8 + column;
+}
+
+string inline getAlgebraicCoordinateFromIndex(int index)
 {
     return getFileOfSquareIndex(index) + to_string(getRankOfSquareIndex(index));
 }
@@ -204,7 +216,6 @@ public:
     }
 
     //*-----------------------------------------------for testing----------------------------------------------------
-    friend BitBoard maskPawnAttacks(int, int);
     //*-----------------------------------------------for testing----------------------------------------------------
 };
 
@@ -293,7 +304,14 @@ const vector<BitBoard> KNIGHT_ATTACK_TABLE =
 // kingAttacks[squareIndex]
 const vector<BitBoard> KING_ATTACK_TABLE = {770ULL, 1797ULL, 3594ULL, 7188ULL, 14376ULL, 28752ULL, 57504ULL, 49216ULL, 197123ULL, 460039ULL, 920078ULL, 1840156ULL, 3680312ULL, 7360624ULL, 14721248ULL, 12599488ULL, 50463488ULL, 117769984ULL, 235539968ULL, 471079936ULL, 942159872ULL, 1884319744ULL, 3768639488ULL, 3225468928ULL, 12918652928ULL, 30149115904ULL, 60298231808ULL, 120596463616ULL, 241192927232ULL, 482385854464ULL, 964771708928ULL, 825720045568ULL, 3307175149568ULL, 7718173671424ULL, 15436347342848ULL, 30872694685696ULL, 61745389371392ULL, 123490778742784ULL, 246981557485568ULL, 211384331665408ULL, 846636838289408ULL, 1975852459884544ULL, 3951704919769088ULL, 7903409839538176ULL, 15806819679076352ULL, 31613639358152704ULL, 63227278716305408ULL, 54114388906344448ULL, 216739030602088448ULL, 505818229730443264ULL, 1011636459460886528ULL, 2023272918921773056ULL, 4046545837843546112ULL, 8093091675687092224ULL, 16186183351374184448ULL, 13853283560024178688ULL, 144959613005987840ULL, 362258295026614272ULL, 724516590053228544ULL, 1449033180106457088ULL, 2898066360212914176ULL, 5796132720425828352ULL, 11592265440851656704ULL, 4665729213955833856ULL};
 
-BitBoard maskPawnAttacks(int playerColor, int squareIndex)
+const vector<BitBoard> BISHOP_ATTACK_TABLE =
+    {18049651735527936ULL, 70506452091904ULL, 275415828992ULL, 1075975168ULL, 38021120ULL, 8657588224ULL, 2216338399232ULL, 567382630219776ULL, 9024825867763712ULL, 18049651735527424ULL, 70506452221952ULL, 275449643008ULL, 9733406720ULL, 2216342585344ULL, 567382630203392ULL, 1134765260406784ULL, 4512412933816832ULL, 9024825867633664ULL, 18049651768822272ULL, 70515108615168ULL, 2491752130560ULL, 567383701868544ULL, 1134765256220672ULL, 2269530512441344ULL, 2256206450263040ULL, 4512412900526080ULL, 9024834391117824ULL, 18051867805491712ULL, 637888545440768ULL, 1135039602493440ULL, 2269529440784384ULL, 4539058881568768ULL, 1128098963916800ULL, 2256197927833600ULL, 4514594912477184ULL, 9592139778506752ULL, 19184279556981248ULL, 2339762086609920ULL, 4538784537380864ULL, 9077569074761728ULL, 562958610993152ULL, 1125917221986304ULL, 2814792987328512ULL, 5629586008178688ULL, 11259172008099840ULL, 22518341868716544ULL, 9007336962655232ULL, 18014673925310464ULL, 2216338399232ULL, 4432676798464ULL, 11064376819712ULL, 22137335185408ULL, 44272556441600ULL, 87995357200384ULL, 35253226045952ULL, 70506452091904ULL, 567382630219776ULL, 1134765260406784ULL, 2832480465846272ULL, 5667157807464448ULL, 11333774449049600ULL, 22526811443298304ULL, 9024825867763712ULL, 18049651735527936ULL};
+
+const vector<BitBoard> ROOK_ATTACK_TABLE =
+    {282578800148862ULL, 565157600297596ULL, 1130315200595066ULL, 2260630401190006ULL, 4521260802379886ULL, 9042521604759646ULL, 18085043209519166ULL, 36170086419038334ULL, 282578800180736ULL, 565157600328704ULL, 1130315200625152ULL, 2260630401218048ULL, 4521260802403840ULL, 9042521604775424ULL, 18085043209518592ULL, 36170086419037696ULL, 282578808340736ULL, 565157608292864ULL, 1130315208328192ULL, 2260630408398848ULL, 4521260808540160ULL, 9042521608822784ULL, 18085043209388032ULL, 36170086418907136ULL, 282580897300736ULL, 565159647117824ULL, 1130317180306432ULL, 2260632246683648ULL, 4521262379438080ULL, 9042522644946944ULL, 18085043175964672ULL, 36170086385483776ULL, 283115671060736ULL, 565681586307584ULL, 1130822006735872ULL, 2261102847592448ULL, 4521664529305600ULL, 9042787892731904ULL, 18085034619584512ULL, 36170077829103616ULL, 420017753620736ULL, 699298018886144ULL, 1260057572672512ULL, 2381576680245248ULL, 4624614895390720ULL, 9110691325681664ULL, 18082844186263552ULL, 36167887395782656ULL, 35466950888980736ULL, 34905104758997504ULL, 34344362452452352ULL, 33222877839362048ULL, 30979908613181440ULL, 26493970160820224ULL, 17522093256097792ULL, 35607136465616896ULL, 9079539427579068672ULL, 8935706818303361536ULL, 8792156787827803136ULL, 8505056726876686336ULL, 7930856604974452736ULL, 6782456361169985536ULL, 4485655873561051136ULL, 9115426935197958144ULL};
+
+BitBoard
+maskPawnAttacks(int playerColor, int squareIndex)
 {
     BitBoard result;
     result.checkSquareIndexValidity(squareIndex);
@@ -424,6 +442,96 @@ BitBoard maskKingAttacks(int squareIndex)
     return result;
 }
 
+BitBoard maskBishopAttacks(int squareIndex)
+{
+    BitBoard result;
+    result.checkSquareIndexValidity(squareIndex);
+
+    int targetIndex = -1;
+
+    int currentRank = getRankOfSquareIndex(squareIndex);
+    char currentFile = getFileOfSquareIndex(squareIndex);
+
+    int rank = 0;
+    char file = 0;
+
+    //*upper right diagonal
+    for (rank = currentRank + 1, file = currentFile + 1; rank <= 7 && file <= 'g'; rank++, file++)
+    {
+        targetIndex = getIndexFromRankAndFile(rank, file);
+        result.setBitAt(targetIndex);
+    }
+
+    //*lower right diagonal
+    for (rank = currentRank - 1, file = currentFile + 1; rank >= 2 && file <= 'g'; rank--, file++)
+    {
+        targetIndex = getIndexFromRankAndFile(rank, file);
+        result.setBitAt(targetIndex);
+    }
+
+    //*upper left diagonal
+    for (rank = currentRank + 1, file = currentFile - 1; rank <= 7 && file >= 'b'; rank++, file--)
+    {
+        targetIndex = getIndexFromRankAndFile(rank, file);
+        result.setBitAt(targetIndex);
+    }
+
+    //*lower left diagonal
+    for (rank = currentRank - 1, file = currentFile - 1; rank >= 2 && file >= 'b'; rank--, file--)
+    {
+        targetIndex = getIndexFromRankAndFile(rank, file);
+        result.setBitAt(targetIndex);
+    }
+
+    return result;
+}
+
+BitBoard maskRookAttacks(int squareIndex)
+{
+    BitBoard result;
+    result.checkSquareIndexValidity(squareIndex);
+
+    int targetIndex = -1;
+
+    int currentRank = getRankOfSquareIndex(squareIndex);
+    char currentFile = getFileOfSquareIndex(squareIndex);
+
+    // cout << "\ncurrentRank = " << currentRank;
+
+    int rank = currentRank;
+    char file = currentFile;
+
+    //*upper file
+    for (rank = currentRank + 1; rank <= 7; rank++)
+    {
+        targetIndex = getIndexFromRankAndFile(rank, currentFile);
+        result.setBitAt(targetIndex);
+    }
+
+    //*lower file
+    for (rank = currentRank - 1; rank >= 2; rank--)
+    {
+        targetIndex = getIndexFromRankAndFile(rank, currentFile);
+        result.setBitAt(targetIndex);
+    }
+
+    //*left rank
+    for (file = currentFile - 1; file >= 'b'; file--)
+    {
+        targetIndex = getIndexFromRankAndFile(currentRank, file);
+        result.setBitAt(targetIndex);
+    }
+
+    //*right rank
+    for (file = currentFile + 1; file <= 'g'; file++)
+    {
+        targetIndex = getIndexFromRankAndFile(currentRank, file);
+        result.setBitAt(targetIndex);
+    }
+
+    return result;
+}
+
 void printPawnAttackTables()
 {
     cout << "\nwhite pawn attacks : \n";
@@ -518,6 +626,28 @@ vector<BitBoard> getKingAttackTable()
     return kingAttackTable;
 }
 
+vector<BitBoard> getBishopAttackTable()
+{
+    vector<BitBoard> bishopAttackTable(64, 0ULL);
+    for (int squareIndex = 0; squareIndex < 64; squareIndex++)
+    {
+        bishopAttackTable[squareIndex] = maskBishopAttacks(squareIndex);
+    }
+
+    return bishopAttackTable;
+}
+
+vector<BitBoard> getRookAttackTable()
+{
+    vector<BitBoard> rookAttackTable(64, 0ULL);
+    for (int squareIndex = 0; squareIndex < 64; squareIndex++)
+    {
+        rookAttackTable[squareIndex] = maskRookAttacks(squareIndex);
+    }
+
+    return rookAttackTable;
+}
+
 void printKingAttacktable(const vector<BitBoard> &kingAttackTable)
 {
     for (int squareIndex = 0; squareIndex < 64; squareIndex++)
@@ -526,15 +656,15 @@ void printKingAttacktable(const vector<BitBoard> &kingAttackTable)
         kingAttackTable[squareIndex].print();
     }
 
-    // cout << "{ ";
-    // for (int squareIndex = 0; squareIndex < 64; squareIndex++)
-    // {
+    cout << "{ ";
+    for (int squareIndex = 0; squareIndex < 64; squareIndex++)
+    {
 
-    //     cout << kingAttackTable[squareIndex].getDecimalValue() << "ULL";
-    //     if (squareIndex != 63)
-    //         cout << ", ";
-    // }
-    // cout << " } \n";
+        cout << kingAttackTable[squareIndex].getDecimalValue() << "ULL";
+        if (squareIndex != 63)
+            cout << ", ";
+    }
+    cout << " } \n";
 }
 
 // void initializeLeaperPieceAttackTables()
